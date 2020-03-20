96b3b45cd98bec1096987de22d5565fd7fb33dbd
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-20 12:54:45 -0700

    Added class getters and methods

--- vampire.js
+++ vampire.js
@@ -10,21 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let currentVampire = this;
+    let generation = 0;
+
+    while (currentVampire.creator) {
+      generation++;
+      currentVampire = currentVampire.creator;
+    }
+
+    return generation;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
 
   }
 
@@ -36,7 +49,33 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampire = this;
+    let comparingVampire = vampire;
 
+    while (currentVampire.isMoreSeniorThan(comparingVampire)) {
+      comparingVampire = comparingVampire.creator;
+      if (currentVampire === comparingVampire) {
+        return currentVampire;
+      }
+    }
+
+    while (comparingVampire.isMoreSeniorThan(currentVampire)) {
+      currentVampire = currentVampire.creator;
+      if (comparingVampire === currentVampire) {
+        return comparingVampire;
+      }
+    }
+
+    while (currentVampire !== comparingVampire) {
+      comparingVampire = comparingVampire.creator;
+      currentVampire = currentVampire.creator;
+      if (currentVampire === comparingVampire) {
+        return currentVampire;
+      }
+
+    }
+
+    return currentVampire;
   }
 }
 
